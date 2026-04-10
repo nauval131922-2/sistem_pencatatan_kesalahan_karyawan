6d6fa469c2b3a93b495376be1d9f9d778a0930c0
Nauval Gunawan <nauvalgunawan5@gmail.com>
2026-04-10 19:27:55 +0700

    fix(config): Hapus devIndicators.appIsrStatus dan buildActivity yang tidak valid di Next.js 15

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devIndicators: {
-    appIsrStatus: false,
-    buildActivity: false,
-  },
+
   experimental: {
     proxyClientMaxBodySize: '500mb',
     serverActions: {
